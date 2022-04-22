6ea6749e356c30a9ba9131589ca252caa8bd3fed
Ujjawal Singh <96330274+uksingh19@users.noreply.github.com>
2022-04-22 14:37:02 +0530

    Create main.py

---
+++ 16-OOP-Coffee-Machine/main.py
@@ -0,0 +1,25 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+should_continue = True
+while should_continue:
+    user_choice = input(f"What do you want to have? ({menu.get_items()}): ")
+
+    if user_choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+
+    elif user_choice == "off":
+        should_continue = False
+
+    else:
+        drink = menu.find_drink(user_choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+
+
