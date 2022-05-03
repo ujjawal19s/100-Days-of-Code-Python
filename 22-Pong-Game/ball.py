2010d6a191679ecace07664eacfbd5f9317d74f3
Ujjawal Singh <96330274+uksingh19@users.noreply.github.com>
2022-05-03 09:46:01 +0530

    Create ball.py

---
+++ 22-Pong-Game/ball.py
@@ -0,0 +1,31 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.height = 20
+        self.width = 20
+        self.color("white")
+        self.move_speed = 0.1
+        self.penup()
+        self.x_move = 10
+        self.y_move = 10
+
+    def move(self):
+        xcor = self.xcor() + self.x_move
+        ycor = self.ycor() + self.y_move
+        self.goto(xcor, ycor)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.bounce_x()
